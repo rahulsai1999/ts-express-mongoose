5bc178e5095d606aa4c793f0d0a8445c4982df26
rahulsai1999 <noneuser2183@gmail.com>
2020-08-17 11:27:16 +0530

    add to set protected route

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Middleware, Post } from "@overnightjs/core";
 import { Request, Response } from "express";
 import { isEmpty } from "lodash";
 import Blog, { BlogType } from "../models/blog";
+import User from "../models/user";
 import isLoggedIn from "../utils/isLoggedIn";
+import extractID from "../utils/extract";
 
 @Controller("blogs")
 export default class BlogController {
@@ -10,12 +12,18 @@ export default class BlogController {
   @Middleware(isLoggedIn)
   createBlog = async (req: Request, res: Response) => {
     try {
+      const payload = extractID(req);
       const blog: BlogType = req.body;
       const { title, text } = blog;
+      const { id } = payload.body;
       if (isEmpty(title) || isEmpty(text)) {
         res.json({ error: true, message: "Either title or text is missing" });
       } else {
         const createdBlog = await Blog.create(blog);
+        await User.findOneAndUpdate(
+          { _id: id },
+          { $addToSet: { blogs: createdBlog._id } }
+        );
         res.json({ error: false, createdBlog });
       }
     } catch (e) {
